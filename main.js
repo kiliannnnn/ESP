1b3d25ec75420b1cfcd4736100145aed185a1410
kiliannnnn <106062878+kiliannnnn@users.noreply.github.com>
2024-05-26 18:16:26 -0400

    removed tags traductor

--- main.js
+++ main.js
@@ -4,8 +4,6 @@
 //     document.childNodes[1].appendChild(document.createElement('script')).setAttribute('src', componentsPath.concat('header.js'));
 // };
 
-var converter = new htmltodom();
-
 document.addEventListener('loadstart', function () {
     if (localStorage.getItem('config') == null) {
         localStorage.setItem('config', JSON.stringify(config));
@@ -15,31 +13,32 @@ document.addEventListener('loadstart', function () {
     }
 });
 
-function download(data, filename, type) {
-    var file = new Blob([data], { type: type });
-    if (window.navigator.msSaveOrOpenBlob) // IE10+
-        window.navigator.msSaveOrOpenBlob(file, filename);
-    else { // Others
-        var a = document.createElement("a"),
-            url = URL.createObjectURL(file);
-        a.href = url;
-        a.download = filename;
-        document.body.appendChild(a);
-        a.click();
-        setTimeout(function () {
-            document.body.removeChild(a);
-            window.URL.revokeObjectURL(url);
-        }, 0);
-    }
-}
+// function download(data, filename, type) {
+//     var file = new Blob([data], { type: type });
+//     if (window.navigator.msSaveOrOpenBlob) // IE10+
+//         window.navigator.msSaveOrOpenBlob(file, filename);
+//     else { // Others
+//         var a = document.createElement("a"),
+//             url = URL.createObjectURL(file);
+//         a.href = url;
+//         a.download = filename;
+//         document.body.appendChild(a);
+//         a.click();
+//         setTimeout(function () {
+//             document.body.removeChild(a);
+//             window.URL.revokeObjectURL(url);
+//         }, 0);
+//     }
+// }
 
-function HTMLToDOM(html) {
-    var data = {
-        src: html,
-    };
-    var output = converter.convert(data);
-    return output;
-}
+// var converter = new htmltodom();
+// function HTMLToDOM(html) {
+//     var data = {
+//         src: html,
+//     };
+//     var output = converter.convert(data);
+//     return output;
+// }
 
 function execCommandWithHTML(command, value) {
     document.execCommand('formatBlock', true, 'div');
